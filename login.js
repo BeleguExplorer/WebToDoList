060bd03fcdf11c968c1795df641a961c93cc345c
BeleguExplorer <163163576+BeleguExplorer@users.noreply.github.com>
2024-03-12 22:03:42 +0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("form")
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    const username = form.username.value 
+    const password = form.password.value 
+
+    const authenticated = authentication(username,password)
+
+    if(authenticated){
+        window.location.href = "index.html"
+    }else{
+        alert("Wrong!")
+    }
+})
+
+//function for checking username & password
+
+function authentication(username,password)
+{
+    if(username === "Mahtabul" && password === "amikgfshourav123" ){
+        return true
+    }else{
+        return false
+    }
+
+}
